a4990db5fc34cb7ce0e5be817bfb67250302b77c
Raccoon-JK <110690389+Raccoon-JK@users.noreply.github.com>
2022-09-08 23:53:02 +0900

    class 정의

---
+++ Programming/ClassApp.java
@@ -0,0 +1,17 @@
+
+public class ClassApp {
+
+	public static void main(String[] args) {
+		
+		// Math = class 연관된 변수와 매소드를 그루핑 한 것
+		
+		System.out.println(Math.PI);
+		
+		System.out.println(Math.floor(1.6));
+		
+		System.out.println(Math.ceil(1.6));
+		
+		
+	}
+
+}
