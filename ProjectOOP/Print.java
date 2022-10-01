080553fb5aaff2591415adab74823123ae46a0fe
Raccoon-JK <110690389+Raccoon-JK@users.noreply.github.com>
2022-10-01 22:21:28 +0900

    수정

--- ProjectOOP/Print.java
+++ ProjectOOP/Print.java
@@ -11,18 +11,3 @@ class Print{
         System.out.println("B");
     }
 }
-public class MyOOP {
-    public static void main(String[] args) {
-        Print.delimiter = "----";
-        Print.A();
-        Print.A();
-        Print.B();
-        Print.B();
-         
-        Print.delimiter = "****";
-        Print.A();
-        Print.A();
-        Print.B();
-        Print.B();
-    }
-}
