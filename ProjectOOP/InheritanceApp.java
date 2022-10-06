02c1fc48584bee853d7c16dcf27037d34c7babff
Raccoon-JK <110690389+Raccoon-JK@users.noreply.github.com>
2022-10-06 23:03:14 +0900

    상속2

--- ProjectOOP/InheritanceApp.java
+++ ProjectOOP/InheritanceApp.java
@@ -1,23 +1,40 @@
 class Cal{
-    int v1,v2;
-    Cal(int v1, int v2){
-        System.out.println("Cal init!!");
-        this.v1 = v1; this.v2 = v2;
+    public int sum(int v1, int v2){
+        return v1+v2;
     }
-    public int sum(){return this.v1+v2;}
+    // Overloading
+    public int sum(int v1, int v2, int v3){
+        return v1+v2+v3;
+    }
 }
 class Cal3 extends Cal{
-    Cal3(int v1, int v2) {
-        super(v1, v2);
-        System.out.println("Cal3 init!!");
+    public int minus(int v1, int v2){
+        return v1-v2;
     }
-    public int minus(){return this.v1-v2;}
+    // Overriding
+    public int sum(int v1, int v2){
+        System.out.println("Cal3!!");
+        return v1+v2;
+    }
+ 
 }
 public class InheritanceApp {
     public static void main(String[] args) {
-        Cal c = new Cal(2,1);
-        Cal3 c3 = new Cal3(2, 1);
-        System.out.println(c3.sum()); // 3
-        System.out.println(c3.minus()); // 1
+        Cal c = new Cal();
+        System.out.println(c.sum(2,1));
+        System.out.println(c.sum(2,1,1));
+ 
+        Cal3 c3 = new Cal3();
+        System.out.println(c3.sum(2,1));
+        System.out.println(c3.minus(2,1));
+        System.out.println(c3.sum(2,1));
+    }
+}
+class Cal2{
+    public int sum(int v1, int v2){
+        return v1+v2;
+    }
+    public int minus(int v1, int v2){
+        return v1-v2;
     }
 }
