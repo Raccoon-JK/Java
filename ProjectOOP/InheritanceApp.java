899e5af21c4d47dd384078f92ff4f4969fb17216
Raccoon-JK <110690389+Raccoon-JK@users.noreply.github.com>
2022-10-07 22:41:27 +0900

    상속

--- ProjectOOP/InheritanceApp.java
+++ ProjectOOP/InheritanceApp.java
@@ -1,11 +1,10 @@
-
 class Cal{
     public int sum(int v1, int v2){
         return v1+v2;
     }
     // Overloading
     public int sum(int v1, int v2, int v3){
-        return v1+v2+v3;
+        return this.sum(v1,v2)+v3;
     }
 }
 class Cal3 extends Cal{
@@ -15,7 +14,7 @@ class Cal3 extends Cal{
     // Overriding
     public int sum(int v1, int v2){
         System.out.println("Cal3!!");
-        return v1+v2;
+        return super.sum(v1,v2);
     }
  
 }
