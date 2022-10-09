2f3c633f0885d784a9f478f9463ef0a50984a9c7
Raccoon-JK <110690389+Raccoon-JK@users.noreply.github.com>
2022-10-09 22:55:42 +0900

    객체지향 인스턴스 활용

--- java-flow-control/AccountingApp.java
+++ java-flow-control/AccountingApp.java
@@ -1,24 +1,19 @@
-
-public class AccountingApp {
-    // 공급가액
-    public static double valueOfSupply = 10000.0;
-    // 부가가치세율
+class Accounting{
+    public static double valueOfSupply;
     public static double vatRate = 0.1;
- 
     public static double getVAT() {
         return valueOfSupply * vatRate;
     }
-     
     public static double getTotal() {
         return valueOfSupply + getVAT();
     }
- 
+}
+public class AccountingApp {
     public static void main(String[] args) {
- 
-        System.out.println("Value of supply : " + valueOfSupply);
-        System.out.println("VAT : " + getVAT());
-        System.out.println("Total : " + getTotal());
- 
+        Accounting.valueOfSupply = 10000.0;
+        System.out.println("Value of supply : " + Accounting.valueOfSupply);
+        System.out.println("VAT : " + Accounting.getVAT());
+        System.out.println("Total : " + Accounting.getTotal());
+  
     }
- 
 }
