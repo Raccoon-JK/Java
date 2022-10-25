37d7ba15bea3c78a7ce1edd29e7a7124e1d059d4
Raccoon-JK <110690389+Raccoon-JK@users.noreply.github.com>
2022-10-25 10:03:06 +0900

    공부

---
+++ ProjectOOP/parallelogram.java
@@ -0,0 +1,15 @@
+
+public class parallelogram {
+	public static void main(String[] args) {
+		// 작성 시작
+		for(int i=0; i<5; i++) {
+			for(int j=0; j<i; j++) {
+				System.out.print(' ');
+			}
+			for(int j=0; j<5; j++) {
+				System.out.print(' ');
+			}
+			System.out.println();
+			}
+		}
+	}
